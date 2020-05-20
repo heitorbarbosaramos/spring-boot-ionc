5947b1dcdb341391f5858ddc9a27fd6a41830c84
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2020-05-20 08:08:46 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/heitor/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/heitor/cursomc/resources/CategoriaResources.java
@@ -1,17 +1,28 @@
 package com.heitor.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.heitor.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value = "/categoria")
+@RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<String> testeResouces() {
-		System.out.println("Teste OK");
-		return ResponseEntity.ok().body("Teste OK");
+	public ResponseEntity<List<Categoria>> testeResouces() {
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(new Categoria(1, "Informática"));
+		lista.add(new Categoria(1, "Escritório"));
+
+		
+		return ResponseEntity.ok().body(lista);
 	}
 }
