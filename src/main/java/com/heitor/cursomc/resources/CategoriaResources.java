a7a94a42a3bd7413ce746a5a6831af28bb9e4bf5
Heitor Ramos <heitorhfbr@gmail.com>
2021-03-06 20:18:01 -0300

    Adcionando a anotacao no insert de uma nova Categoria

--- src/main/java/com/heitor/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/heitor/cursomc/resources/CategoriaResources.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +37,7 @@ public class CategoriaResources {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<?> insert(Categoria obj){
+	public ResponseEntity<?> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
